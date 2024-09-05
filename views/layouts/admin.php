2f8a5964ebc6b83e0a6d3d2c8ca0b8cac0a96857
Юрій Боровський <yurgenoa@MacBook-Air-Urij.local>
2024-09-05 11:59:08 +0300

    end

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -60,14 +60,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     </div>
 </main>
 
-<footer id="footer" class="mt-auto py-3 bg-light">
-    <div class="container">
-        <div class="row text-muted">
-            <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
-            <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
-        </div>
-    </div>
-</footer>
+<?= $this->render('_footer') ?>
 
 <?php $this->endBody() ?>
 </body>
